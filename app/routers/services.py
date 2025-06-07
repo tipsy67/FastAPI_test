42c6e051448250786244e4ece1e4589991e4facc
dan.toropov <dan.toropov@gmail.com>
2025-06-07 23:14:00 +0300

    Add comments

--- app/routers/services.py
+++ app/routers/services.py
@@ -1,15 +1,14 @@
-from typing import Annotated
+from typing import Annotated, Callable
 from fastapi import Depends, HTTPException
-from sqlalchemy import Boolean
 from starlette import status
 
 from app.models import Product
 from app.routers.auth import get_current_user
 
 
-def check_user_permissions(permissions: list[str]):
-    """Фабрика зависимостей"""
-    async def dependency(get_user: Annotated[dict, Depends(get_current_user)]):
+def check_user_permissions(permissions: list[str]) -> Callable:
+    """ Фабрика зависимостей для проверки прав пользователей """
+    async def dependency(get_user: Annotated[dict, Depends(get_current_user)]) -> dict:
         for permission in permissions:
             if not get_user.get(permission):
                 raise HTTPException(
@@ -22,6 +21,7 @@ def check_user_permissions(permissions: list[str]):
     return dependency
 
 async def calculate_rank(product: Product, add: bool, grade: int) -> float:
+    """ Подсчет рейтинга продукта при добавлении оценки """
     reviews_count = product.reviews_count or 0
     if add:
         new_rating = (product.rating * reviews_count + grade)/(reviews_count + 1)
